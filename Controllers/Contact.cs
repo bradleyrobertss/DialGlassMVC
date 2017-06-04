3f86eb9fbfa9b8f0c198e163f50ff79555f80675
Bradley Roberts <bradleyrobertss@gmail.com>
2017-06-04 19:21:18 +0100

    Initial Check In

---
+++ Controllers/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DialGlass.Controllers
+{
+    public class Contact : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+    }
+}
